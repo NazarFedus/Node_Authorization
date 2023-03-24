fcef776b8319b1a2860e125172f309455807f102
NazarFedus <simpleprograming7@gmail.com>
2023-03-24 10:17:50 +0000

    add validation to registration

--- authController.js
+++ authController.js
@@ -1,16 +1,22 @@
 const User = require('./models/User');
 const Role = require('./models/Role');
 const bcrypt = require('bcryptjs');
+const {validationResult} = require('express-validator');
 
 class authController {
      async registration(req, res){
           try{
+               const errors = validationResult(req);
+               if(!errors.isEmpty()){
+                    return res.status(400).json({message: "Registration error", errors})
+               }
+
                const {username, password} = req.body; // to get data from the body of the request
                const candidate = await User.findOne({username}); // to find the user in the database
-
                if(candidate){
                     return res.status(400).json({message: "User with this username already exists"})
                }
+               
                const hashPassword = bcrypt.hashSync(password, 7);
                const userRole = await Role.findOne({value: "USER"});
                const newUser = new User({username, password: hashPassword, roles: [userRole.value]});
--- authRouter.js
+++ authRouter.js
@@ -1,8 +1,12 @@
 const Router = require('express');
 const router = new Router();
 const controller = require('./authController')
+const {check} = require('express-validator');
 
-router.post('/registration', controller.registration);
+router.post('/registration', [
+     check('username', "Username can't be empty").notEmpty(),
+     check('password', "Password must be longer than 4 and shorter than 10").isLength({min: 4, max: 10})
+], controller.registration);
 router.post('/login', controller.login);
 
 router.get('/users', controller.getUsers);
